ed609fafcefc0a1166fada76af608e5cdc1e9e5f
fanglenn02 <glenn.fan.2023@scis.smu.edu.sg>
2024-10-05 14:39:22 +0800

    RestClient methods.

--- Project/src/main/java/csd/cuemaster/client/RestTemplateClient.java
+++ Project/src/main/java/csd/cuemaster/client/RestTemplateClient.java
@@ -1,60 +1,60 @@
 package csd.cuemaster.client;
 
-
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
-import csd.cuemaster.profile.Book;
+import java.util.List;
+
+import csd.cuemaster.profile.Profile;
 
 @Component
 public class RestTemplateClient {
-    
     private final RestTemplate template;
 
     /**
      * Add authentication information for the RestTemplate
-     * 
      */
     public RestTemplateClient(RestTemplateBuilder restTemplateBuilder) {
         this.template = restTemplateBuilder
                 .basicAuthentication("admin", "goodpassword")
                 .build();
     }
+
     /**
-     * Get a book with given id
-     * 
+     * Returns a sorted list of players with given id.
      * @param URI
      * @param id
      * @return
      */
-    public Book getBook(final String URI, final Long id) {
-        final Book book = template.getForObject(URI + "/" + id, Book.class);
-        return book;
+    public List<Profile> getLeaderboard(final String URI, final Long id) {
+        ResponseEntity<List<Profile>> response = template.exchange(
+        URI + "/" + id,
+        HttpMethod.GET,
+        null,
+        new ParameterizedTypeReference<List<Profile>>() {}
+        );
+        List<Profile> profiles = response.getBody();
+        return profiles;
     }
 
     /**
-     * Add a new book
-     * 
-     * @param URI
-     * @param newBook
-     * @return
-     */
-    public Book addBook(final String URI, final Book newBook) {
-        final Book returned = template.postForObject(URI, newBook, Book.class);
-        
-        return returned;
-    }
-
-    /**
-     * Get a book, but return a HTTP response entity.
+     * Returns a list of players after a points reset with given id.
      * @param URI
      * @param id
      * @return
      */
-    public ResponseEntity<Book> getBookEntity(final String URI, final Long id){
-        return template.getForEntity(URI + "/{id}", Book.class, Long.toString(id));
+    public List<Profile> resetPoints(final String URI, final Long id) {
+        ResponseEntity<List<Profile>> response = template.exchange(
+        URI + "/" + id,
+        HttpMethod.PUT,
+        null,
+        new ParameterizedTypeReference<List<Profile>>() {}
+        );
+        List<Profile> profiles = response.getBody();
+        return profiles;
     }
-    
 }
