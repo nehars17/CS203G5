2d85465ebe16c538332971001bc830104c190726
nehars17 <nehars.rs@gmail.com>
2024-10-20 18:11:07 +0800

    Added Match Codes

--- Project/src/main/java/csd/cuemaster/match/MatchRepository.java
+++ Project/src/main/java/csd/cuemaster/match/MatchRepository.java
@@ -1,5 +1,7 @@
 package csd.cuemaster.match;
 
+import java.util.List; 
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,4 +9,9 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface MatchRepository extends JpaRepository <Match, Long> {
 
+    // Custom query to find all matches by tournament ID
+    List<Match> findByTournamentId(Long tournamentId);
+
+    // Custom query to find matches by user ID (player)
+    List<Match> findByUser1IdOrUser2Id(Long userId1, Long userId2);
 }
---
+++ Project/src/main/java/csd/cuemaster/match/MatchService.java
@@ -0,0 +1,14 @@
+package csd.cuemaster.match;
+
+import java.util.List;
+
+public interface MatchService {
+
+    Match createMatch(Match match);
+    Match updateMatch(Long id, Match match);
+    Match getMatchById(Long matchId);
+    Match declareWinner(Long matchId, Long winnerId);
+    List<Match> getAllMatches();
+    void deleteMatchById(Long matchId);
+    List<Match> getMatchesByTournamentId(Long tournamentId);
+}
