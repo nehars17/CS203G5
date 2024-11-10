258216a9dd38753da0cdbd1afa502f492de370ac
nehars17 <nehars.rs@gmail.com>
2024-11-10 23:32:13 +0800

    Update index.tsx

--- cuemaster/src/index.tsx
+++ cuemaster/src/index.tsx
@@ -1,16 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom'; // Change to react-dom for React 17
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
+// Use ReactDOM.render() for React 17
+ReactDOM.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById('root') // No need for 'as HTMLElement'
 );
 
 // If you want to start measuring performance in your app, pass a function
